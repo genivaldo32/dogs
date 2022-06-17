c94e06498cbe095f3ddc50c42fbce7d3a14aaddf
Genivaldo <genivaldocicero32@gmail.com>
2022-06-17 10:23:15 -0300

    Autenticando_formulario

--- src/components/Login/LoginForm.js
+++ src/components/Login/LoginForm.js
@@ -1,36 +1,47 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { useForm } from "../../Hooks/useForm";
-import { Button } from "../Forms/Button";
 import { Input } from "../Forms/Input";
+import { Button } from "../Forms/Button";
+import { useForm } from "../../Hooks/useForm";
+import { TOKEN_POST, USER_GET } from "../../api";
 
 export const LoginForm = () => {
   const username = useForm();
   const password = useForm();
 
-  function handleSubmit(ev) {
+  React.useEffect(() => {
+    const token = window.localStorage.getItem("token");
+    if (token) {
+      getUser(token);
+    }
+  }, []);
+
+  async function getUser(token) {
+    const { url, options } = USER_GET(token);
+    const response = await fetch(url, options);
+    const json = await response.json();
+    console.log(json);
+  }
+
+  async function handleSubmit(ev) {
+    ev.preventDefault();
+
     if (username.validate() && password.validate()) {
-      ev.preventDefault();
-      fetch("https://dogsapi.origamid.dev/json/jwt-auth/v1/token", {
-        method: "POST",
-        headers: {
-          "content-Type": "application/json",
-        },
-        body: JSON.stringify(),
-      })
-        .then((response) => {
-          console.log(response);
-          return response.json();
-        })
-        .then((json) => {
-          console.log(json);
-        });
+      const { url, options } = TOKEN_POST({
+        username: username.value,
+        password: password.value,
+      });
+
+      const response = await fetch(url, options);
+      const json = await response.json();
+      window.localStorage.setItem("token", json.token);
+      getUser(json.token);
     }
   }
 
   return (
     <section>
-      <h1>Login</h1>
+      <h1>LoginForm</h1>
       <form action="" onSubmit={handleSubmit}>
         <Input label="Usuário" type="text" name="username" {...username} />
         <Input label="Senha" type="password" name="password" {...password} />
