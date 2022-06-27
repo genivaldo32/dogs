acdb902c5e1dbd2b9045a3d2e497cc0900992f85
Genivaldo <genivaldocicero32@gmail.com>
2022-06-26 23:23:23 -0300

    finalizando o User Heaer

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ import { ReactComponent as Dogs } from "../Assets/dogs.svg";
 import { UserContext } from "../UserContext";
 
 export const Header = () => {
-  const { data, userLogOut } = React.useContext(UserContext);
+  const { data } = React.useContext(UserContext);
 
   return (
     <header className={Styles.header}>
@@ -16,7 +16,6 @@ export const Header = () => {
         {data ? (
           <Link className={Styles.login} to="/conta">
             {data.nome}
-            <button onClick={userLogOut}>Sair</button>
           </Link>
         ) : (
           <Link className={Styles.login} to="/login">
