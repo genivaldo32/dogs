32232c56e1b6594ed36df6728aa673c0044a54cb
Genivaldo <genivaldocicero32@gmail.com>
2022-07-12 19:55:09 -0300

    Biblioteca externa

--- src/components/User/UserStats.js
+++ src/components/User/UserStats.js
@@ -1,12 +1,31 @@
 import React from "react";
+import { STATS_GET } from "../../api";
+import { useFetch } from "../../Hooks/useFetch";
 import { Head } from "../Helpers/Head";
+import { Loading } from "../Helpers/Loading";
+import { Error } from "../Helpers/Error";
+import { UsetStatsGraphs } from "./UsetStatsGraphs";
 
 const UserStats = () => {
-  return (
-    <div>
-      <Head title="Estatísticas" />
-    </div>
-  );
+  const { data, error, loading, request } = useFetch();
+  React.useEffect(() => {
+    async function getData() {
+      const { url, options } = STATS_GET();
+      await request(url, options);
+    }
+    getData();
+  }, [request]);
+
+  if (loading) return <Loading />;
+  if (error) return <Error error={error} />;
+  if (data)
+    return (
+      <div>
+        <Head title="Estatísticas" />
+        <UsetStatsGraphs data={data} />
+      </div>
+    );
+  else return null;
 };
 
 export default UserStats;
